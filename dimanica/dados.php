4db1834dfe4186cec35e11b550764888a14f0a47
gabriel <gabrielslolihhjj@gmail.com>
2019-10-16 15:34:29 -0300

    calculando peso

--- dimanica/dados.php
+++ dimanica/dados.php
@@ -1,9 +1,9 @@
 <?php   
-$n1 = $_GET['numero1'];
+$n1 = @$_GET['numero1'];
 
-$n2 = $_GET['numero2'];
+$n2 = @$_GET['numero2'];
 
-$n3 = $_GET['numero3'];
+$n3 = @$_GET['numero3'];
 
 $resultado = ($n1 + $n2 + $n3) /3;
 
---
+++ dimanica/form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<form class="form-horizontal" action="calcular.php" method="post">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Peso ideal</legend>
+
+<!-- Text input-->
+<div class="form-group" >
+  <label class="col-md-4 control-label" for="altura">Altura</label>  
+  <div class="col-md-4">
+  <input id="altura" name="altura" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Multiple Radios -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="sexo">Sexo</label>
+  <div class="col-md-4">
+  <div class="radio">
+    <label for="sexo-0">
+      <input type="radio" name="sexo" id="sexo-0" value="1" checked="checked">
+      Masculino
+    </label>
+	</div>
+  <div class="radio">
+    <label for="sexo-1">
+      <input type="radio" name="sexo" id="sexo-1" value="2">
+      Feminino
+    </label>
+	</div>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="calcular"></label>
+  <div class="col-md-4">
+    <button id="calcular" name="calcular" class="btn btn-primary">Calcular</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+    
+</body>
+</html>
